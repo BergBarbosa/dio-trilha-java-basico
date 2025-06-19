6d24015ea29d1b54a46dd27d42080485e020b522
BergBarbosa <wandemberg_dsb@hotmail.com>
2025-06-19 20:18:01 -0300

    simulando abertura da conta bancária

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("CRIE UMA CONTA NO BANCO DIO!");
+        System.out.printf("****************************\n");
+
+        System.out.print("Por favor, digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite o número da agência: ");
+        String agencia = sc.next();
+        System.out.print("Digite o número da conta: ");
+        String conta = sc.next();
+        double saldo = 237.48;
+        System.out.println();
+
+        System.out.println("Olá " + nome + ", obrigado por criar um conta em nosso banco!");
+        System.out.println("Sua agência é: " + agencia + ", conta: " + conta + " e seu saldo é: " + saldo);
+
+
+        sc.close();
     }
 }
